f1ca90249368fea5a04244c55db72517a5879006
Luis Silvera <luisma0520@hotmail.com>
2014-10-08 12:22:07 -0500

    Fix details model user and views

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,6 +3,7 @@ class Player < ActiveRecord::Base
     @id_user = id_user
     return who_did_you_played if played?
     return false if available_users.empty?
+    id_user_2 = available_users
     id_user_2 = id_user_2[rand(id_user_2.length)]
     Player.create(user_id_1: @id_user, user_id_2: id_user_2)
     Player.create(user_id_1: id_user_2, user_id_2: @id_user)
@@ -25,10 +26,10 @@ class Player < ActiveRecord::Base
   def who_did_you_played
     name_user(find_user_by_id.user_id_2)
   end
-  def name_user(id)
-    User.find(id).email
-  end
   private
+    def name_user(id)
+      User.find(id).email
+    end
     def find_user_by_id
       Player.find_by(user_id_1: @id_user)
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007160344) do
+ActiveRecord::Schema.define(version: 20141008162937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20141007160344) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
